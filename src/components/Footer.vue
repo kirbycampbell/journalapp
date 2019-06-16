8e9031c50104b5c4907d0abb635894a7cede6cf5
kirbycampbell <jkirbycampbell@gmail.com>
2019-06-15 17:03:59 -0700

    Footer Max Set

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,29 @@
     <template>
   <div class="footer">
-    <div class="footer-logo">Council On Foreign Relations</div>
-    <div class="footer-button">
-      <div class="btn-item">About CFR</div>
-      <div class="btn-item">Support CFR</div>
-      <div class="btn-item">Careers</div>
-    </div>
-    <div class="footer-button">
-      <div class="btn-item">Think Tank</div>
-      <div class="btn-item">Media</div>
-      <div class="btn-item">Contact Us</div>
-    </div>
-    <div class="news-sec">
-      <h2>Daily News Brief</h2>
-      <p>Sign up for a morning roundup of news and analysis from around the world.</p>
-    </div>
-    <div class="form-area">
-      <input class="email-form" placeholder="Email Address" type="email" name="emailaddress">
-      <input class="email-submit" type="submit">
-    </div>
-    <div class="bottom-of-footer">
-      ©2019 Councilon Foreign Relations. All rights reserved. Privacy Policy and Terms of Use.
-      <br>Kirby Campbell Clone.
+    <div class="footer-max">
+      <div class="footer-logo">Council On Foreign Relations</div>
+      <div class="footer-button">
+        <div class="btn-item">About CFR</div>
+        <div class="btn-item">Support CFR</div>
+        <div class="btn-item">Careers</div>
+      </div>
+      <div class="footer-button">
+        <div class="btn-item">Think Tank</div>
+        <div class="btn-item">Media</div>
+        <div class="btn-item">Contact Us</div>
+      </div>
+      <div class="news-sec">
+        <h2>Daily News Brief</h2>
+        <p>Sign up for a morning roundup of news and analysis from around the world.</p>
+      </div>
+      <div class="form-area">
+        <input class="email-form" placeholder="Email Address" type="email" name="emailaddress">
+        <input class="email-submit" type="submit">
+      </div>
+      <div class="bottom-of-footer">
+        ©2019 Councilon Foreign Relations. All rights reserved. Privacy Policy and Terms of Use.
+        <br>Kirby Campbell Clone.
+      </div>
     </div>
   </div>
 </template>
